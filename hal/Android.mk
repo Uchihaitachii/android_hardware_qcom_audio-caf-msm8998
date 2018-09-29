f08b65748237aa12760ed81d16f049c33a007caf
Alexandre Rangel <32802713+mamutal91@users.noreply.github.com>
2018-09-29 14:11:16 +0000

    Update Android.mk

--- hal/Android.mk
+++ hal/Android.mk
@@ -9,7 +9,7 @@ LOCAL_ARM_MODE := arm
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter msm8974 msm8226 msm8610 apq8084 msm8994 msm8992 msm8996 msm8998 apq8098_latv sdm845,$(TARGET_BOARD_PLATFORM)),)
-  # B-family platform uses msm8974 code base
+  # BB-family platform uses msm8974 code base
   AUDIO_PLATFORM = msm8974
   MULTIPLE_HW_VARIANTS_ENABLED := true
 ifneq ($(filter msm8610,$(TARGET_BOARD_PLATFORM)),)
